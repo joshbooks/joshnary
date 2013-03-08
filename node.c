f54120f6d77a38ab50ce0c50ae2bccf4ab01cf3d
Josh Hight <joshbooks@gmail.com>
2013-03-08 00:30:50 -0800

    this seems to work and significantly improve performance with the sequences that normally generate unbalanced trees

--- node.c
+++ node.c
@@ -51,9 +51,19 @@ struct node* insert(struct node* node, int datum)
 		if (datum < node->data)
 			if (node->left == NULL)
 			{
-				node->left = newNode(datum);
-				node->left->parent=node;
-				return node->left;
+				if (node->right==NULL && node->parent->right==NULL)
+				{
+					node->parent->right=newNode(node->parent->data);
+					node->parent->data = node->data;
+					node->data = datum;
+					return node;
+				}
+				else
+				{
+					node->left = newNode(datum);
+					node->left->parent=node;
+					return node->left;
+				}
 			}
 			else
 				node = node->left;
@@ -61,9 +71,19 @@ struct node* insert(struct node* node, int datum)
 		else if (datum > node->data)
 			if (node->right == NULL)
 			{
-				node->right = newNode(datum);
-				node->right->parent = node;
-				return node->right;
+				if (node->left==NULL && node->parent->left==NULL)
+				{
+					node->parent->left=newNode(node->parent->data);
+					node->parent->data = node->data;
+					node->data = datum;
+					return node;
+				}
+				else
+				{
+					node->right = newNode(datum);
+					node->right->parent = node;
+					return node->right;
+				}
 			}
 			else
 				node = node->right;
@@ -92,26 +112,28 @@ struct node* find(struct node* start, int value)
 int
 main(void)
 {
-	struct node *n1 = newNode(500);
-//	n1->right = newNode(10);
-//	n1->right->parent = n1;
-//	n1->left = newNode(4);
-//	n1->left->parent = n1;
-//	insert(n1, 7);
+	struct node *n1 = newNode(5);
+
+//	printf ("%i", insert(n1, 5)->data);
+	n1->right = newNode(10);
+	n1->right->parent = n1;
+	n1->left = newNode(4);
+	n1->left->parent = n1;
 //	insert(n1, 9);
+//	insert(n1, 7);
 //	insert(n1, 3);
 //	insert(n1, 6);
-//	int i;
-//	for (i=0; i<10000; i++)
-//		insert (n1, i);
+	int i;
+	for (i=0; i<10000; i++)
+		insert (n1, i);
 
-//	for (i=0; i<10000; i++)
-//		printf("%i\n", find(n1,i)->data);
+	for (i=0; i<10000; i++)
+		printf("%i\n", find(n1,i)->data);
 //	printf ("%i\n", top(n1)->left->right->data);
 //	printf ("%i\n", top(n1)->left->data);
-//	printf ("%i\n", top(n1)->right->right->data);
 //	printf ("%i\n", top(n1)->data);
-
-	//printf ("%i\n", n1->data);
+//	printf ("%i\n", top(n1)->right->data);
+//	printf ("%i\n", top(n1)->right->right->data);
+//	printf ("%i\n", top(n1)->right->left->right->data);
 	return 0;
 }
