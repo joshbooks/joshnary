186532aacb9a6b69f6542237f615eb9fc280a559
Josh Hight <joshbooks@gmail.com>
2013-03-11 19:12:11 -0700

    cleared up some stupid stuff, no longer have to manually add first three nodes

--- node.c
+++ node.c
@@ -29,8 +29,9 @@ struct node* newNode(int datum)
 	return edon;
 }
 
-//performs one extra check at the bottom, seems to eliminate the long-branch
-//unbalanced tree, the zigzag is a little less significant, but it
+//performs one extra check at the bottom, seems to alleviate the long-branch
+//unbalanced tree a bit be nice to eliminate it, the zigzag is a little less
+//significant, but it
 //would be nice to deal with that too
 
 struct node* insert(struct node* node, int datum)
@@ -41,7 +42,7 @@ struct node* insert(struct node* node, int datum)
 		if (datum < node->data)
 			if (node->left == NULL)
 			{
-				if (node->right==NULL && node->parent->right==NULL)
+				if ( node->parent!=NULL && node->right==NULL && node->parent->right==NULL)
 				{
 					node->parent->right=newNode(node->parent->data);
 					node->parent->data = node->data;
@@ -61,7 +62,7 @@ struct node* insert(struct node* node, int datum)
 		else if (datum > node->data)
 			if (node->right == NULL)
 			{
-				if (node->left==NULL && node->parent->left==NULL)
+				if (node->parent!=NULL && node->left==NULL && node->parent->left==NULL)
 				{
 					node->parent->left=newNode(node->parent->data);
 					node->parent->data = node->data;
@@ -106,25 +107,31 @@ main(void)
 	struct node *n1 = newNode(5);
 
 //	printf ("%i", insert(n1, 5)->data);
-	n1->right = newNode(10);
-	n1->right->parent = n1;
-	n1->left = newNode(4);
-	n1->left->parent = n1;
+//	n1->right = newNode(10);
+//	n1->right->parent = n1;
+//	n1->left = newNode(4);
+//	n1->left->parent = n1;
 //	insert(n1, 9);
 //	insert(n1, 7);
 //	insert(n1, 3);
 //	insert(n1, 6);
 	int i;
+//inserting a bunch sequentially is a great way to unbalance
 	for (i=0; i<10000; i++)
 		insert (n1, i);
-
-//	for (i=0; i<10000; i++)
-//		printf("%i\n", find(n1, i)->data);
-//	printf ("%i\n", top(n1)->left->right->data);
-//	printf ("%i\n", top(n1)->left->data);
+//make sure I haven't screwed up tree structure
+	for (i=0; i<10000; i++)
+		printf("%i\n", find(n1, i)->data);
+	printf ("%i\n", top(n1)->left->right->data);
+	printf ("%i\n", top(n1)->left->data);
 	printf ("%i\n", top(n1)->data);
 	printf ("%i\n", top(n1)->right->data);
-	printf ("%i\n", top(n1)->right->right->right->data);
-	printf ("%i\n", top(n1)->right->right->left->data);
+//	printf ("%i\n", top(n1)->right->right->right->data);
+//	printf ("%i\n", top(n1)->right->right->left->data);
+
+	printf ("%i\n", n1->left->right->data);
+	printf ("%i\n", n1->left->data);
+	printf ("%i\n", n1->data);
+	printf ("%i\n", n1->right->data);
 	return 0;
 }
