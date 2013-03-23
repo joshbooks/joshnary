9b99631da93533d0a02c51b5710aa9c1bbe788a5
Josh Hight <joshbooks@gmail.com>
2013-03-22 23:41:03 -0700

    stupidly forgot to add grrr

---
+++ node.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node node;
+
+struct node
+{
+	int data;
+	node *left;
+	node *right;
+	node *parent;
+};
+
+struct node* max(struct node*);
+
+struct node* min(struct node*);
+
+struct node* top(struct node*);
+
+struct node* newNode(int);
+
+struct node* insert(struct node*, int);
+
+struct node* find(struct node*, int);
